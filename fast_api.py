d672883fa4d54728ac0217a4eff8521a6d188e5b
amuluscode <amulthapa991@gmail.com>
2025-08-07 10:18:00 +0200

    Update fast_api.py

--- fast_api.py
+++ fast_api.py
@@ -1,32 +1,45 @@
-from fastapi import FastAPI, HTTPException,Path,Query
-import json
-app=FastAPI()
-def load_data():
-    with open("patients.json", "r") as f:
-        data = json.load(f)
-    return data
-@app.get("/") 
-async def root() -> dict[str,str]:
-    return {"message": "this is a fully functional fastapi app"}
-@app.get("/view")
-async def about()  :
-    data=load_data()
-    return data
-@app.get("/patient/{patient_id}")
-async def get_patient(patient_id: str= Path(..., description="The ID of the patient to retrieve",example='P001')):
-    data=load_data()
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import google.generativeai as genai
+from fastapi.middleware.cors import CORSMiddleware
+
+GEMINI_API_KEY = 'Enter Your own API-key'
+
+genai.configure(api_key = GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*'],
+)
+
+class ContentRequest(BaseModel):
+    topic : str
+    tone: str = 'informative'
+    length : str = "medium"
+
+
+@app.post("/generate")
+
+def generate_content(req: ContentRequest):
+    prompt = f"""
+            Write a {req.length} piece of content on the topic: '{req.topic}'.
+            The topic should be {req.tone}
+            Make it creative, clear, and engaging and in simple language
+        """
     
-    if  patient_id in data:
-            return data[patient_id]
-    HTTPException(status_code=404, detail="Patient not found")
-@app.get('/sort')
-def sort_values(sort_by: str =Query(...,description="sort by age,name,height,bmi",example='age'),order: str = Query('asc', description="Order of sorting: asc or desc", example='asc')):
-    valid_data=['height','age','name','bmi','weight']
-    if sort_by not in valid_data:
-        raise HTTPException(status_code=400, detail=f"Invalid sort_by parameter{valid_data}")
-    if order not in ['asc', 'desc']:
-        raise HTTPException(status_code=400, detail="Order must be 'asc' or 'desc'")
-    data = load_data()
-    sorted_data = sorted(data.values(), key=lambda x: x[sort_by], reverse=False if order == 'asc' else True)
-    return sorted_data
-   
+    try:
+        response = model.generate_content(prompt)
+        print(f"Generated content: {response.text}")
+        return {"generate_content": response.text}
+    
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = str(e))
